84ef120e91ae6577026fa34cfb642d9ba7d7ce56
ABDULLAHALSAKKA <49825628+ABDULLAHALSAKKA@users.noreply.github.com>
2019-05-29 23:12:53 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo -e "sutup ABDULLAH ALSAKKA "
+pip2 install request
+pip2 install mechanize
+pip2 install requirements
+pip install --upgrade pip
+pkg install pip
+pkg install nano
+pip install mechanize
+pip2 install --upgrade pip
+pip install --upgrade pip
